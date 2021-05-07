30df536efb2cc6c09e96c0fcfb49ecb79ee6ad3c
Aubrey Bennett <27aubreyb@gmail.com>
2021-05-07 18:20:41 -0400

    added page routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/navbar";
+import Wrapper from "./components/Wrapper/wrapper";
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import NewGroup from "./pages/newgroup";
+import GroupPage from "./pages/grouppage";
+import Invite from "./pages/invitepage";
+import Transactions from "./pages/transaction";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/newgroup" component={NewGroup} />
+          <Route exact path="/grouppage" component={GroupPage} />
+          <Route exact path="/invitepage" component={Invite} />
+          <Route exact path="/transaction" component={Transactions} />
+        </Wrapper>
+      </div>
+    </Router>
   );
 }
 
