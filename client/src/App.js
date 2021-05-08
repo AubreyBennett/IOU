b385ab75613adc002c90b89e6bc97fdfb47aef2b
xena <74474948+a-andres1@users.noreply.github.com>
2021-05-08 13:05:26 -0400

    imported signup page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/navbar";
 import Wrapper from "./components/Wrapper/wrapper";
 import Login from "./pages/login";
+import Signup from "./pages/signup";
 import Dashboard from "./pages/dashboard";
 import NewGroup from "./pages/newgroup";
 import GroupPage from "./pages/grouppage";
@@ -16,6 +17,7 @@ function App() {
         <Navbar />
         <Wrapper>
           <Route exact path="/" component={Login} />
+          <Route exact path="/signup" component={Signup} />
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/newgroup" component={NewGroup} />
           <Route exact path="/grouppage" component={GroupPage} />
