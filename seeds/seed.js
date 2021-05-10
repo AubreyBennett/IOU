6856d5e5c3f909c980c15a7d8e5a3463079585f4
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-05-09 22:24:16 -0400

    created see for relations table

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,15 +1,17 @@
 const sequelize = require("../config/connection");
-const { Transaction, User, Circle } = require("../models");
+const { Transaction, User, Circle, UserCircle } = require("../models");
 
 const transactionSeedData = require("./transactionSeedData.json");
 const userSeedData = require("./userSeedData.json");
 const circleSeedData = require("./circleSeedData.json");
+const userCircleSeedData = require("./userCircleSeedData.json");
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
     const transactions = await Transaction.bulkCreate(transactionSeedData)
     const users = await User.bulkCreate(userSeedData);
     const circles = await Circle.bulkCreate(circleSeedData);
+    const userCircles = await UserCircle.bulkCreate(userCircleSeedData);
     process.exit(0);
 }
 
