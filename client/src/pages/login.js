56a64e6c0437b8289322e2b6206caa3664cb11dd
xena <74474948+a-andres1@users.noreply.github.com>
2021-05-11 20:47:33 -0400

    corrected syntax

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -3,19 +3,19 @@ import React from 'react';
 function Login() {
   return (
 
-    <div class="container col-lg-10 fs-4">
-      <div class="row align-items-center">
-        <div class="col-lg-3"></div>
-        <div class="col-lg-6">
-          <form class="p-4 p-md-5 border rounded-5 bg-info">
-            <div class="form-floating mb-3">
-              <input class="form-control" type="email" placeholder="Email address" aria-label="default input example"></input>
+    <div className="container col-lg-10 fs-4">
+      <div className="row align-items-center">
+        <div className="col-lg-3"></div>
+        <div className="col-lg-6">
+          <form className="p-4 p-md-5 border rounded-5 bg-info">
+            <div className="form-floating mb-3">
+              <input className="form-control" type="email" placeholder="Email address" aria-label="default input example"></input>
             </div>
-            <div class="form-floating mb-3">
-              <input class="form-control" type="password" placeholder="Password" aria-label="default input example"></input>
+            <div className="form-floating mb-3">
+              <input className="form-control" type="password" placeholder="Password" aria-label="default input example"></input>
             </div>
-            <div class="mt-5">
-              <button class="w-100 btn btn-lg btn-info" type="submit">Login</button>
+            <div className="mt-5">
+              <button className="w-100 btn btn-lg btn-info" type="submit">Login</button>
             </div>
           </form>
         </div>
