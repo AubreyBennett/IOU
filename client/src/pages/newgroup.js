e1ce25132372f705a5ee8861d27b2abcc068b00a
Aubrey Bennett <27aubreyb@gmail.com>
2021-05-11 21:51:13 -0400

    added button route to newgroup.js

--- client/src/pages/newgroup.js
+++ client/src/pages/newgroup.js
@@ -3,15 +3,18 @@ import React from 'react';
 function NewGroup() {
   return (
     <>
-    <div className="card" style= {{textAlign: "center", backgroundColor: "blue", marginBottom: "50px"}}>
-      <h1>New Group Page</h1>
-    </div>
+      <div className="card" style={{ textAlign: "center", backgroundColor: "blue", marginBottom: "50px" }}>
+        <h1>New Group Page</h1>
+      </div>
       <div className="input-group mb-3">
         <input type="text" className="form-control" placeholder="Enter Group Name" aria-label="Enter Group Name" aria-describedby="button-addon2" />
-          <button className="w-20 btn btn-lg btn-info" type="button" id="button-addon2">Submit</button>
+        <button className="w-20 btn btn-lg btn-info" type="button" onClick={(e) => {
+          e.preventDefault();
+          window.location.pathname = '/grouppage';
+        }} id="button-addon2">Submit</button>
       </div>
-      </>
-    );
-  }
+    </>
+  );
+}
 
-  export default NewGroup;
+export default NewGroup;
