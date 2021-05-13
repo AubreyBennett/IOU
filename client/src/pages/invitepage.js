d67b0cd2ae5c852ab3cca5124e828e82e2a88f07
Aubrey Bennett <27aubreyb@gmail.com>
2021-05-13 19:09:00 -0400

    basic invite page html layout

--- client/src/pages/invitepage.js
+++ client/src/pages/invitepage.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 function Invite() {
-    return <h1> Invite Page </h1>;
-  }
-  
-  export default Invite;
+  return (
+    <>
+      <div className="card" style={{ textAlign: "center", backgroundColor: "blue", marginBottom: "50px" }}>
+        <h1>Invite Page</h1>
+      </div>
+      <div className="input-group mb-3">
+        <input type="text" className="form-control" placeholder="First Name" aria-label="Enter Group Name" />
+      </div>
+      <div className="input-group mb-3">
+        <input type="text" className="form-control" placeholder="Last Name" aria-label="Enter Group Name" />
+      </div>
+      <div className="input-group mb-3">
+        <input type="text" className="form-control" placeholder="Email" aria-label="Enter Group Name" />
+      </div>
+      <button className="w-20 btn btn-lg btn-info" type="button" onClick={(e) => {
+        e.preventDefault();
+        window.location.pathname = '/invitepage';
+      }} id="button-addon2">Submit</button>
+    </>
+  );
+}
+
+export default Invite;
