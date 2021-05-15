203d9556dc80cae37f5bb9f91e93b3a4bbad1027
Aubrey Bennett <27aubreyb@gmail.com>
2021-05-15 11:32:16 -0400

    put group page HTML in the right place

--- client/src/pages/grouppage.js
+++ client/src/pages/grouppage.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
 function GroupPage() {
-    return <h1> Group Page </h1>;
-  }
-  
-  export default GroupPage;
+  return (
+    <>
+      <div className="card bg-info" style={{ textAlign: "center", backgroundColor: "blue", marginBottom: "50px" }}>
+        <h1>My Group Page</h1>
+      </div>
+      <div class="card text-center" style={{ marginBottom: "50px"}}>
+        <div class="card-header">
+          My Transactions
+      </div>
+        <div class="card-body">
+          <p class="card-text">Where all the transactions go</p>
+        </div>
+      </div>
+      <div class="card text-center" style={{ marginBottom: "50px"}}>
+        <div class="card-body">
+          <p class="card-text">Math stuff</p>
+        </div>
+      </div>
+      <button className="w-20 btn btn-lg btn-info" type="button" onClick={(e) => {
+        e.preventDefault();
+        window.location.pathname = '/transaction';
+      }} id="button-addon2">Pay Now</button>
+      <button className="w-20 btn btn-lg btn-info" type="button" onClick={(e) => {
+        e.preventDefault();
+        window.location.pathname = '/invitepage';
+      }} id="button-addon2">Invite</button>
+    </>
+  );
+}
+
+export default GroupPage;
