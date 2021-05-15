4a89cf342a41bd0b70341bc199a2ff1cfc297a1c
Aubrey Bennett <27aubreyb@gmail.com>
2021-05-15 11:34:21 -0400

    fixed newgroup page

--- client/src/pages/newgroup.js
+++ client/src/pages/newgroup.js
@@ -3,30 +3,18 @@ import React from 'react';
 function NewGroup() {
   return (
     <>
-      <div className="card bg-info" style={{ textAlign: "center", backgroundColor: "blue", marginBottom: "50px" }}>
+      <div className="card bg-info" style={{ textAlign: "center", marginBottom: "50px" }}>
+
+
         <h1>New Group Page</h1>
       </div>
-      <div class="card text-center" style={{ marginBottom: "50px"}}>
-        <div class="card-header">
-          My Transactions
-      </div>
-        <div class="card-body">
-          <p class="card-text">Where all the transactions go</p>
-        </div>
-      </div>
-      <div class="card text-center" style={{ marginBottom: "50px"}}>
-        <div class="card-body">
-          <p class="card-text">Math stuff</p>
-        </div>
+      <div className="input-group mb-3">
+        <input type="text" className="form-control" placeholder="Enter Group Name" aria-label="Enter Group Name" />
       </div>
       <button className="w-20 btn btn-lg btn-info" type="button" onClick={(e) => {
         e.preventDefault();
-        window.location.pathname = '/transaction';
-      }} id="button-addon2">Pay Now</button>
-      <button className="w-20 btn btn-lg btn-info" type="button" onClick={(e) => {
-        e.preventDefault();
-        window.location.pathname = '/invitepage';
-      }} id="button-addon2">Invite</button>
+        window.location.pathname = '/grouppage';
+      }} id="button-addon2">Submit</button>
     </>
   );
 }
