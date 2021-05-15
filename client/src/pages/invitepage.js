6cb458f844f9a9152003cc9614ed44c66be3606b
Aubrey Bennett <27aubreyb@gmail.com>
2021-05-15 11:44:51 -0400

    added dropdown for the group you want to invite to

--- client/src/pages/invitepage.js
+++ client/src/pages/invitepage.js
@@ -15,6 +15,14 @@ function Invite() {
       <div className="input-group mb-3">
         <input type="text" className="form-control" placeholder="Email" aria-label="Email" />
       </div>
+      <label for="groups">Choose a Group Name:</label>
+
+      <select name="groups" id="cars">
+        <option value="group1">Group 1</option>
+        <option value="group2">Group 2</option>
+        <option value="group3">Group 3</option>
+        <option value="group4">Group 4</option>
+      </select>
       <button className="w-20 btn btn-lg btn-info" type="button" onClick={(e) => {
         e.preventDefault();
         window.location.pathname = '/invitepage';
