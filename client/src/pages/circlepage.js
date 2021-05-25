1918e00fc9b0a670b775f90bff41f8c66d63deba
xena <74474948+a-andres1@users.noreply.github.com>
2021-05-24 22:13:45 -0400

    created handlers and api calls

--- client/src/pages/circlepage.js
+++ client/src/pages/circlepage.js
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function CirclePage() {
+  // creating state to capture all the circle transactions
+  const [transactionState, newTransactionState] = useState();
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch('/api/transactions', {
+      method: "POST", 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        ...transactionState
+      })
+    })
+    .then((data) => {
+      console.log("Transaction successful");
+    })
+    .catch();
+  }
+
+  function handleTransaction(e){
+    
+  }
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }
+    fetch(`/api/transactions/circle/user`, options)
+      .then(res => res.json())
+      .then(data => { console.log(data)}
+      ).catch(err => {
+        console.log(err)
+      })
+
+  }, []);
+
+
+    
+
+
+
+
   return (
     <>
       <div className="card bg-info" style={{ textAlign: "center", backgroundColor: "blue", marginBottom: "50px" }}>
         <h1>My Group Page</h1>
       </div>
-      <div className="card text-center" style={{ marginBottom: "50px"}}>
+      <div className="card text-center" style={{ marginBottom: "50px" }}>
         <div className="card-header">
           My Transactions
       </div>
@@ -14,12 +61,28 @@ function CirclePage() {
           <p className="card-text">Where all the transactions go</p>
         </div>
       </div>
-      <div className="card text-center" style={{ marginBottom: "50px"}}>
+      <div className="card text-center" style={{ marginBottom: "50px" }}>
         <div className="card-body">
           <p className="card-text">Math stuff</p>
         </div>
       </div>
-      <button className="w-20 btn btn-lg btn-info" type="button" onClick={(e) => {
+      <label for="cars">Choose a Circle Name:</label>
+
+      <select name="circle" id="circle">
+        <option value="volvo">Group 1</option>
+        <option value="saab">Group 2</option>
+        <option value="mercedes">Group 3</option>
+        <option value="audi">Group 4</option>
+      </select>
+      <div className="input-group mb-3">
+        <input type="text" className="form-control" placeholder="Enter Payment Description" aria-label="Enter Payment Description" />
+      </div>
+      <div className="input-group mb-3">
+        <input type="text" className="form-control" placeholder="Enter Payment Amount" aria-label="Enter Payment Amount" />
+      </div>
+      <button className="w-20 btn btn-lg btn-info" 
+      type="button" 
+      onClick={(e) => {
         e.preventDefault();
         window.location.pathname = '/transaction';
       }} id="button-addon2">Pay Now</button>
