36dbc18ddecd6f61fdd5c6a4dc438dcf8d400d21
xena <74474948+a-andres1@users.noreply.github.com>
2021-05-27 16:39:22 -0400

    updated component so that it renders individual circles

--- client/src/components/Dashboard/dashboardcomponent.js
+++ client/src/components/Dashboard/dashboardcomponent.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
 
-function CircleMap({data}) {
+function CircleMap({ data }) {
 
-    console.log("--------------------------/////---------------")
-    console.log(data)
+    const [submitState, setSubmitState] = useState({
 
+        circleID: 0,
+        users: []
+    
+      });
+
+   
     return (
         <>
+        <button className="card text-dark bg-info mb-3 rounded-circle mt-3" style={{ maxWidth: '20rem', justifyContent: "center" }}
+        onClick={(e) => {
+            setSubmitState({
+              ...submitState, circleID: parseInt(e.target.value)
+            });
+            window.location.href = './circlepage'
+          }}>
+          <div className="card-body">
             <div className="card-body">
-                <p className="card-text">{data.name} </p>
+                <p className="card-text"  style={{ textAlign: 'center' }} >{data.name} </p>
             </div>
-
+          </div>
+        </button>
         </>
     );
 
